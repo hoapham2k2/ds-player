30a54fec1c2450c11f6486f2564163a4e36c7f70
hoapham2k2 <20520995@gm.uit.edu.vn>
2024-07-09 17:13:51 +0700

    update

--- src/main/services/fetchContentsAsync.js
+++ src/main/services/fetchContentsAsync.js
@@ -9,23 +9,26 @@ import LoadConfigurationFile from '../utils/loadConfigurationFile'
 
 export default async function FetchContentsAndSaveToLocalAsync() {
   const appConfig = await LoadConfigurationFile()
+  const deviceId = await GetDeviceID()
+  console.log('Device ID:', deviceId)
   let latestFetchingTime = ''
   if (appConfig.latestFetchingTime === undefined) {
     latestFetchingTime = '1970-12-07T10:16:40.000Z'
   } else {
     latestFetchingTime = appConfig.latestFetchingTime
   }
+  console.log('Latest fetching time:', latestFetchingTime)
 
   try {
     const { data, error } = await supabase.rpc('get_content_items', {
       deviceid: GetDeviceID(),
-      previoustime: latestFetchingTime
+      previoustime: latestFetchingTime.toString()
     })
     if (error) {
       console.error('Error fetching contents:', error.message)
       return []
     }
-    console.log('Content Items belong to the device:', data)
+    console.log('Content Items belong to the device:', JSON.stringify(data, null, 2))
 
     //saving to local media path
     const mediaSavePath = GetApplicationMediaPath()
@@ -47,11 +50,11 @@ export default async function FetchContentsAndSaveToLocalAsync() {
     }
 
     for (const item of data) {
-      const filePath = item.file_path
+      const filePath = item.file_path //bucketName/fileName
       const bucketName = filePath.split('/')[0].trim()
       const fileName = filePath.slice(bucketName.length + 1).trim()
       console.log(`is downloading ${fileName} from ${bucketName} bucket`)
-      const { data, error } = await supabase.storage.from('content').download(filePath)
+      const { data, error } = await supabase.storage.from(bucketName).download(fileName)
       if (error) {
         console.error('Error downloading file from Supabase Storage:', error)
         continue
